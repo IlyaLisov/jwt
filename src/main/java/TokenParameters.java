e0ece6c6b45b8adff523deed10462543b85375a9
Ilya Lisov <ilya.lisov.yt@gmail.com>
2024-01-17 18:03:39 +0300

    #2 Implement service

---
+++ src/main/java/TokenParameters.java
@@ -0,0 +1,145 @@
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Builder(
+        builderMethodName = "hiddenBuilder",
+        access = AccessLevel.PRIVATE
+)
+@Getter
+public class TokenParameters {
+
+    /**
+     * A map of claims to be put in JWT token.
+     */
+    private Map<String, Object> claims;
+
+    /**
+     * The "sub" of JWT token.
+     */
+    private String subject;
+
+    /**
+     * Date when JWT token was issued.
+     */
+    private Date issuedAt;
+
+    /**
+     * Date when JWT token will be expired.
+     */
+    private Date expiredAt;
+
+    /**
+     * Creates a builder for TokenParameters.
+     *
+     * @param subject  sub of JWT token
+     * @param duration duration between token issuing and expiration date
+     * @return TokenParametersBuilder
+     */
+    public static TokenParametersBuilder builder(
+            final String subject,
+            final Duration duration
+    ) {
+        Date issuedAt = new Date();
+        return hiddenBuilder()
+                .claims(new HashMap<>())
+                .issuedAt(issuedAt)
+                .subject(subject)
+                .expiredAt(new Date(
+                        issuedAt.getTime()
+                                + 1000 * duration.get(ChronoUnit.SECONDS)
+                ));
+    }
+
+    public static class TokenParametersBuilder {
+
+        /**
+         * Add claims to parameters.
+         *
+         * @param key   the key of claim
+         * @param value the value of claim
+         * @return TokenParametersBuilder
+         */
+        public TokenParametersBuilder claim(
+                final String key,
+                final Object value
+        ) {
+            this.claims.put(key, value);
+            return this;
+        }
+
+        /**
+         * Adds claims to parameters.
+         *
+         * @param claims a map of claims
+         * @return TokenParametersBuilder
+         */
+        public TokenParametersBuilder claims(
+                final Map<String, Object> claims
+        ) {
+            this.claims.putAll(claims);
+            return this;
+        }
+
+        /**
+         * Sets issued date for JWT token.
+         *
+         * @param issuedAt date of issuing
+         * @return TokenParametersBuilder
+         */
+        public TokenParametersBuilder issuedAt(
+                final Date issuedAt
+        ) {
+            this.issuedAt = issuedAt;
+            return this;
+        }
+
+        /**
+         * Sets expiration date for JWT token.
+         *
+         * @param expiredAt date of expiration
+         * @return TokenParametersBuilder
+         */
+        public TokenParametersBuilder expiredAt(
+                final Date expiredAt
+        ) {
+            this.expiredAt = expiredAt;
+            return this;
+        }
+
+        /**
+         * Sets subject to parameters.
+         *
+         * @param subject subject of JWT token
+         * @return TokenParametersBuilder
+         */
+        public TokenParametersBuilder subject(
+                final String subject
+        ) {
+            this.subject = subject;
+            return this;
+        }
+
+        /**
+         * Builds final object.
+         *
+         * @return TokenParameters object
+         */
+        public TokenParameters build() {
+            return new TokenParameters(
+                    claims,
+                    subject,
+                    issuedAt,
+                    expiredAt
+            );
+        }
+
+    }
+
+}
