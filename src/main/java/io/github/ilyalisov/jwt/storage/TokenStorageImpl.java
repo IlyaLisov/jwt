ab2ade0f3a451fadfb49f025030cf423139294ad
Ilya Lisov <ilya.lisov.yt@gmail.com>
2024-01-22 11:56:05 +0300

    #16 Implement token invalidation

--- src/main/java/io/github/ilyalisov/jwt/storage/TokenStorageImpl.java
+++ src/main/java/io/github/ilyalisov/jwt/storage/TokenStorageImpl.java
@@ -1,9 +1,14 @@
 package io.github.ilyalisov.jwt.storage;
 
 import io.github.ilyalisov.jwt.config.TokenParameters;
+import lombok.Getter;
 
+import java.util.Date;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Basic implementation of TokenStorage.
@@ -13,7 +18,12 @@ public class TokenStorageImpl implements TokenStorage {
     /**
      * Inner map of key-value pairs.
      */
-    private final ConcurrentHashMap<String, String> tokens;
+    private final ConcurrentHashMap<String, TokenEntry> tokens;
+
+    /**
+     * Scheduled executor for cleanup tokens.
+     */
+    private final ScheduledExecutorService scheduler;
 
     /**
      * Creates an object.
@@ -21,6 +31,23 @@ public class TokenStorageImpl implements TokenStorage {
     public TokenStorageImpl(
     ) {
         this.tokens = new ConcurrentHashMap<>();
+        this.scheduler = Executors.newSingleThreadScheduledExecutor();
+        scheduler.scheduleAtFixedRate(
+                () -> {
+                    Date date = new Date();
+                    for (Map.Entry<
+                            String,
+                            TokenEntry
+                            > entry : tokens.entrySet()) {
+                        if (entry.getValue().isAfter(date)) {
+                            tokens.remove(entry.getKey());
+                        }
+                    }
+                },
+                0,
+                1,
+                TimeUnit.SECONDS
+        );
     }
 
     private String subjectTokenKey(
@@ -39,8 +66,11 @@ public class TokenStorageImpl implements TokenStorage {
                 params.getSubject(),
                 params.getType()
         );
-        //TODO invalidate tokens - set expiration time
-        tokens.put(tokenKey, token);
+        TokenEntry entry = new TokenEntry(
+                token,
+                params.getExpiredAt()
+        );
+        tokens.put(tokenKey, entry);
     }
 
     @Override
@@ -52,7 +82,11 @@ public class TokenStorageImpl implements TokenStorage {
                 params.getSubject(),
                 params.getType()
         );
-        return token.equals(tokens.get(tokenKey));
+        TokenEntry entry = tokens.get(tokenKey);
+        if (entry == null) {
+            return false;
+        }
+        return token.equals(entry.token);
     }
 
     @Override
@@ -63,7 +97,11 @@ public class TokenStorageImpl implements TokenStorage {
                 params.getSubject(),
                 params.getType()
         );
-        return tokens.get(tokenKey);
+        TokenEntry entry = tokens.get(tokenKey);
+        if (entry == null) {
+            return null;
+        }
+        return entry.token;
     }
 
     @Override
@@ -71,8 +109,8 @@ public class TokenStorageImpl implements TokenStorage {
             final String token
     ) {
         boolean deleted = false;
-        for (Map.Entry<String, String> entry : tokens.entrySet()) {
-            if (entry.getValue().equals(token)) {
+        for (Map.Entry<String, TokenEntry> entry : tokens.entrySet()) {
+            if (entry.getValue().token.equals(token)) {
                 tokens.remove(entry.getKey());
                 deleted = true;
             }
@@ -89,6 +127,35 @@ public class TokenStorageImpl implements TokenStorage {
                 params.getType()
         );
         return tokens.remove(tokenKey) != null;
+    }
+
+    @Getter
+    private static class TokenEntry {
+
+        /**
+         * Token.
+         */
+        private final String token;
+
+        /**
+         * Expiration date.
+         */
+        private final Date expiredAt;
+
+        TokenEntry(
+                final String token,
+                final Date expiredAt
+        ) {
+            this.token = token;
+            this.expiredAt = expiredAt;
+        }
+
+        public boolean isAfter(
+                final Date date
+        ) {
+            return expiredAt.after(date);
+        }
+
     }
 
 }
